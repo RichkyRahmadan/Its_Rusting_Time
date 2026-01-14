e8921df8ad23ec36b1752853730a30681f9f3274
Richky Suaminya Agnes Tachyon <tachrichky@gmail.com>
2026-01-14 21:57:02 +0700

    Ini commit kedua

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[test]
+fn fungsi_test_1(){
+    // About Unit Test
+    println!("Hello, Aku Suaminya Agnes Tachyon");
+}
+#[test]
+fn fungsi_test_2(){
+    // About Immutable Variable
+    let nama = "Richky Rahmadan";
+    println!("Hello, Aku {} Suaminya Gentildonna",nama);
+}
+
+#[test]
+fn fungsi_test_3(){
+    // About mutable variable
+    let mut nama = "Richky Rahmadan";
+    println!("Hello, Aku {} Suaminya Gentildonna",nama);
+    nama = "The King of The Kingless World";
+    println!("Hello, Aku Memiliki Title {}",nama);
+}
